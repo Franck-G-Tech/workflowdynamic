3bab2f6fc56ab020dfc9198bcc519e676d78f56c
BrayanNavaGTech <brayan.nava@g-tech.io>
2025-12-18 16:25:30 -0600

    optimizacion

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,8 @@
 import type { Metadata } from 'next'
 import {
   ClerkProvider,
-  SignedIn,
-  UserButton,
 } from '@clerk/nextjs'
-import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
 
 export const metadata: Metadata = {
   title: 'Clerk Next.js Quickstart',
@@ -29,16 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
           {children}
-        </body>
-      </html>
     </ClerkProvider>
   )
 }
