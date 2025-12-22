c883208fb6fb746b46773f3e1acb6d386dae6013
MarioLiraGTech <mario.lira@g-tech.io>
2025-12-22 12:39:28 -0600

    Rename proxy a middlewere

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,8 +1,9 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
 import { dynamicWorkflow } from "@/inngest/functions";
+import { helloWorld } from "@/inngest/hello";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [dynamicWorkflow],
+  functions: [ dynamicWorkflow, helloWorld ],
 });
