7aa93dcab70177d90e9e79b5b102f47721a73492
MarioLiraGTech <mario.lira@g-tech.io>
2025-12-22 14:13:52 -0600

    Validacion de aprobacion de solicitudes

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,9 +1,8 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
 import { dynamicWorkflow } from "@/inngest/functions";
-import { helloWorld } from "@/inngest/hello";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [ dynamicWorkflow, helloWorld ],
+  functions: [ dynamicWorkflow ],
 });
